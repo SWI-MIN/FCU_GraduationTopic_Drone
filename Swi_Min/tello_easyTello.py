d00b50100af35d398b22c7e16871e3e0a6ba85e9
swimming <swimming881127@gmail.com>
2021-05-15 13:51:27 +0800

    UPLOAD

--- Swi_Min/tello_easyTello.py
+++ Swi_Min/tello_easyTello.py
@@ -8,9 +8,12 @@ myTello.takeoff()
 # myTello.forward(80)
 myTello.streamon() #開啟影像串流
 # myTello.wait(25.55)  # 開啟 wait 會把後面的指令吃掉
-time.sleep(15)         # 用 sleep 不會吃掉後面指令，會先睡完在執行
+time.sleep(10)         # 用 sleep 不會吃掉後面指令，會先睡完在執行
                        # 目前跑起來，當有在移動的時候畫面會變得很糟糕
-# myTello.forward(80)
+print(myTello.get_battery()) #開啟影像串流
+time.sleep(10)
+myTello.forward(80)
+myTello.back(80)
 # myTello.cw(180)
 # myTello.forward(80)
 # myTello.forward(80)
