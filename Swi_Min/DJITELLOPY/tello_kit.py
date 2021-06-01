42c065aa266b4ed343e500ec99098d5b388b3474
swimming <swimming881127@gmail.com>
2021-06-01 20:23:17 +0800

    UPLOAD

--- Swi_Min/DJITELLOPY/tello_kit.py
+++ Swi_Min/DJITELLOPY/tello_kit.py
@@ -1,17 +1,20 @@
 from djitellopy import Tello
-import time
+from time import sleep
 tello = Tello()
 
 tello.connect()
-# time.sleep(1)
+sleep(1)
+print(tello.get_battery())
+sleep(1)
 tello.takeoff()
-# time.sleep(1)
-tello.move_left(150)
-# time.sleep(1)
-tello.rotate_counter_clockwise(180)
-# time.sleep(1)
-tello.move_right(150)
-# time.sleep(1)
-tello.rotate_counter_clockwise(180)
-# time.sleep(1)
+sleep(8)
+
+tello.move_left(25)
+sleep(3)
+tello.move('left',25)
+sleep(3)
+tello.rotate_clockwise(360)
+sleep(8)
+tello.move_right(50)
+sleep(3)
 tello.land()
