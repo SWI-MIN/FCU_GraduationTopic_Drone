1e51c4a8c64b535b13ad77a6ff4497a01e06ff06
SWI-MIN <swimming881127@gmail.com>
2021-07-14 23:15:35 +0800

    test

--- Swi_Min/DJITELLOPY/DJITelloPy.py
+++ Swi_Min/DJITELLOPY/DJITelloPy.py
@@ -1,15 +1,18 @@
 from djitellopy import Tello
 import cv2 
 import time
-tello = Tello()
 
+tello = Tello()
 tello.connect()
 
 print(tello.get_battery())
 tello.streamon()
 while 1:
+    print(1)
     img = tello.get_frame_read().frame
-    img = cv2.resize(img,(360,240))
+    print(2)
+    # img = tello.get_frame_read().frame
+    # img = cv2.resize(img,(360,240))
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
