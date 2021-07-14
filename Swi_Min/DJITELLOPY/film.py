9ed2a8c3c574e5be042583c110fb80a4e08b26ed
swimming <swimming881127@gmail.com>
2021-07-14 23:19:35 +0800

    upload

--- Swi_Min/DJITELLOPY/film.py
+++ Swi_Min/DJITELLOPY/film.py
@@ -8,9 +8,7 @@ tello.connect()
 print(tello.get_battery())
 tello.streamon()
 while 1:
-    print(1)
     img = tello.get_frame_read().frame
-    print(2)
     # img = tello.get_frame_read().frame
     # img = cv2.resize(img,(360,240))
     cv2.imshow("Image", img)
