8c078ecba4f99d401d2523b804b56ffa20fb38ef
SWI-MIN <swimming881127@gmail.com>
2021-07-18 16:26:37 +0800

    upload by SwiMin

--- Swi_Min/DJITELLOPY/film.py
+++ Swi_Min/DJITELLOPY/film.py
@@ -7,10 +7,10 @@ tello.connect()
 
 print(tello.get_battery())
 tello.streamon()
+
 while 1:
     img = tello.get_frame_read().frame
-    # img = tello.get_frame_read().frame
-    # img = cv2.resize(img,(360,240))
+    img = cv2.resize(img,(360,240))
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
---
+++ Swi_Min/capture.py
@@ -0,0 +1,16 @@
+import cv2
+
+while 1:
+
+    video = cv2.VideoCapture(0)
+    matrix, frame = video.read()
+    print(frame)
+    cv2.imshow("Capture", frame)
+    # cv2.waitKey(0)
+    cv2.waitKey(1)
+    # key = cv2.waitKey(1)
+    # if key == ord("q"):
+    #     break
+
+video.release()
+cv2.destroyAllWindows()
