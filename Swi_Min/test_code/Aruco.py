a74722a962bffaa6dc76fe2e43430ccf9bec666e
SWI-MIN <swimming881127@gmail.com>
2021-09-01 23:16:35 +0800

    SwiMin

--- Swi_Min/test_code/Aruco.py
+++ Swi_Min/test_code/Aruco.py
@@ -7,7 +7,7 @@ import pygame
 import csv
 
 # self.angles_tof = [pitch, roll, yaw, tof]
-class Camera():
+class Aruco():
     def __init__(self) -> None:
         self.cam_matrix = None
         self.cam_distortion = None
@@ -20,6 +20,17 @@ class Camera():
         self.marker_size = 10
 
         self.target = TargetDefine()
+
+        self.R_flip  = np.zeros((3,3), dtype=np.float32)
+        self.R_flip[0,0] =  1.0
+        self.R_flip[1,1] = -1.0
+        self.R_flip[2,2] = -1.0
+
+        # about navigation
+        self.navigation_start = False # 這裡需要主程式傳送thread event ，來決定導航狀態是否開啟
+        self.main_marker = None # 記錄誰是主要
+        self.find_new_marker = False # 標記是否需要找尋下一個marker
+        self.used_marker = [] # 存放用過的marker
 
     def aruco(self, frame):
         if np.all(self.cam_matrix== None) or np.all(self.cam_distortion == None):
@@ -39,35 +50,91 @@ class Camera():
         corners, ids, _ = cv2.aruco.detectMarkers(image=gray, dictionary=self.aruco_dict,
                             parameters=self.parameters,cameraMatrix=self.cam_matrix, distCoeff=self.cam_distortion) 
 
-        id_list=[]# 存放當前看到的 ID
-
         if np.all(ids != None):
             ### id found
+            id_list = [] # 存放原始 id 順序
+            sort_id = np.zeros((ids.size, 5), dtype=np.float_) # 存放已經排序過的
             # rvec旋转矩阵、tvec位移矩阵
             rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners,
                                 self.marker_size, self.cam_matrix, self.cam_distortion)
-            
-            # 將所有讀到的 id 加到 id_list 中
-            for i in range(0, ids.size):
-                cv2.aruco.drawAxis(frame, self.cam_matrix, self.cam_distortion, rvecs[i], tvecs[i], 10)  # Draw axis
-                id_list.append(ids[i][0])
-
-                # 測試都看到了哪些ids，並印出來
-                cv2.putText(frame, str(ids[i][0]), (10, (i*20+40)), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 170, 255),1,cv2.LINE_AA)
-                
-                # TargetDefine
-                # Target_ID = self.target.changeTarget(ids[i][0])[0][3]
-                # cv2.putText(frame, str(Target_ID), (10, 500), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 170, 255),1,cv2.LINE_AA)
-                # if Target_ID ==  -1:
-                #     print("tello.land()+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-                    
             # 標記周圍畫正方形
             cv2.aruco.drawDetectedMarkers(frame, corners)
+            # Draw axis and 將有需要用到的資訊存入 sort_id 中 
+            for i in range(0, ids.size):
+                cv2.aruco.drawAxis(frame, self.cam_matrix, self.cam_distortion, rvecs[i], tvecs[i], 10)  # Draw axis
+                ''' sort_id : 
+                        |  id1  |  距離  |  X  |  Y  |  Z  |
+                        |  id2  |  距離  |  X  |  Y  |  Z  |
+                        |  id3  |  距離  |  X  |  Y  |  Z  |
+                '''
+                # 將讀到的 marker id 存到 sort_id 中
+                id_list.append(ids[i][0])
+                sort_id[i][0] = ids[i][0]
+                # 求出各別 marker 到飛機的距離
+                sort_id[i][1] = (tvecs[i][0][0]**2 + tvecs[i][0][1]**2 + tvecs[i][0][2]**2)**0.5
+            
+            # 將 sort_id 距離 由短到近優先，id 由小到大次之排序
+            sort_id = sort_id[np.lexsort((sort_id[:, 0], sort_id[:, 1]))] 
+
+            for i in range(0, ids.size):
+                cv2.putText(frame, str(int(sort_id[i][0]))               , (10, (i*20+40))  , cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 170, 255),1,cv2.LINE_AA)
+                cv2.putText(frame, "D : {:.2f} cm".format(sort_id[i][1]) , (60, (i*20+40))  , cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 170, 255),1,cv2.LINE_AA)
+                cv2.putText(frame, "X : {:+.2f}"  .format(sort_id[i][2]) , (200, (i*20+40)) , cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 170, 255),1,cv2.LINE_AA)
+                cv2.putText(frame, "Y : {:+.2f}"  .format(sort_id[i][3]) , (300, (i*20+40)) , cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 170, 255),1,cv2.LINE_AA)
+                cv2.putText(frame, "Z : {:+.2f}"  .format(sort_id[i][4]) , (400, (i*20+40)) , cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 170, 255),1,cv2.LINE_AA)
+            
+            if self.navigation_start == True:  # 導航開始
+                # 如果 main marker == None，就把最接近飛機的 marker 作為 main
+                # 如果 main marker != None，判斷main_marker是否在used_marker裡面，沒有就加進去
+                if self.main_marker != None:
+                    if self.main_marker not in self.used_marker:
+                        self.used_marker.append(self.main_marker)
+                else:
+                    self.main_marker = sort_id[0][0]
+
+                # 判斷是否需要找新 marker， 不找就畫黃色標示線，並且做動作
+                if not self.find_new_marker:
+                    if self.main_marker not in sort_id[0:,0:1]:
+                        pass
+                    else:
+                        id_index = id_list.index(self.main_marker)
+                        cx = int((corners[id_index][0][0][0]+corners[id_index][0][1][0]+corners[id_index][0][2][0]+corners[id_index][0][3][0])/4)
+                        cy = int((corners[id_index][0][0][1]+corners[id_index][0][1][1]+corners[id_index][0][2][1]+corners[id_index][0][3][1])/4)
+                        cv2.line(frame, (int(w/2), int(h/2)), (cx, cy), (0,255,255), 3)
+                    self.navigation(sort_id)
+                # else 是要找新marker，裡面新增找新marker的要求(條件)
+                else:
+                    # 如果沒有發現新的 marker 就旋轉尋找( 這個部分不一定要擺在這裡 )
+                    pass
+                    # 取得非main marker 中最近的一個，並且不能用過
+                    for i in range(0, ids.size):
+                        new_marker = sort_id[i][0]
+                        if new_marker not in self.used_marker:
+                            self.main_marker = new_marker
+                            self.find_new_marker = False
+                            break
+            
         else:
             ### No id found
             cv2.putText(frame, "No Ids", (10, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 170, 255),1,cv2.LINE_AA)
 
         return frame
+
+    def navigation(self, sort_id):
+        if self.main_marker not in sort_id:
+            pass
+        # 在這裡做完相應動作並且無人機的位置相對於marker已經在指定範圍內以後
+        # 記得要在某些情況下開啟find_new_marker的狀態，才能尋找下一個marker
+        # self.find_new_marker = True
+
+
+        # TargetDefine
+        # Target_ID = self.target.changeTarget(ids[i][0])[0][3]
+        # cv2.putText(frame, str(Target_ID), (10, 500), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 170, 255),1,cv2.LINE_AA)
+        # if Target_ID ==  -1:
+        #     print("tello.land()+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+
 
 # distance from marker in camera Z coordinates
 DIST = 0.9
@@ -108,7 +175,7 @@ def main():
     tello.connect()
     tello.streamon()
 
-    cam = Camera()
+    cam = Aruco()
     
     while True:
         frame = tello.get_frame_read().frame
